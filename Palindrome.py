9d1d9318d5d56ca0bb773951339bd70592a25b7c
Akashdeep <40572037+eogistmaniac@users.noreply.github.com>
2020-08-05 18:27:23 +0530

    Delete Palindrome.py

--- Palindrome.py
+++
@@ -1,9 +0,0 @@
-def Palindrome(s):
-    if(len(s)<=1):
-        return True
-    if(s[0] != s[len(s)-1]):
-        return False
-    else:
-        return(Palindrome(s[1:((len(s))-1)]))
-n = input("Enter the string : ")
-print(Palindrome(n))
