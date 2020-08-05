8535279f4471127f646dce5fa73882ed7ae09707
Akashdeep <akashdeeptiwari44>
2020-08-05 18:16:11 +0530

    Factorial program using tail recursion

---
+++ Palindrome.py
@@ -0,0 +1,9 @@
+def Palindrome(s):
+    if(len(s)<=1):
+        return True
+    if(s[0] != s[len(s)-1]):
+        return False
+    else:
+        return(Palindrome(s[1:((len(s))-1)]))
+n = input("Enter the string : ")
+print(Palindrome(n))
